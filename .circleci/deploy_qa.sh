ac6aca8b52da77315fa80b6ca35626b0a5938c16
Nithin Mallya <nithin@mallya.org>
2017-10-15 01:24:32 -0400

    36

--- .circleci/deploy_qa.sh
+++ .circleci/deploy_qa.sh
@@ -4,12 +4,11 @@ gcloud auth activate-service-account --key-file ${HOME}/gcp-key.json
 gcloud config set project circle-agent
 gcloud container clusters get-credentials circlecicluster --zone us-central1-a --project circle-agent
 
-echo 'kubectl config view'
-kubectl config view
+
 
 echo 'kubectl proxy '
 kubectl proxy --address 0.0.0.0 --accept-hosts '.*' &
-
+sleep 6
 
 echo 'curling now...'
 curl http://localhost:8001/api/v1
